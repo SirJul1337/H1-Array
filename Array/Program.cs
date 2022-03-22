bf5783264cc195bf9bee9dc57b4364a58a11b2e0
nj <niklas@julit.dk>
2022-03-22 12:35:19 +0100

    Add project files.

---
+++ Array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int [9];
+           
+
+         
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1 [i] = (i + 1);
+                if (i == 5)
+                {
+                    array1[i] = i * 2;
+                }
+            }
+            Console.WriteLine(array1[5]);
+         
+        }
+    }
+}
